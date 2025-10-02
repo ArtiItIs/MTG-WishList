3d86c434123f9c2c9294aedebc4a852bb6f2490c
ArtiItIs <olejniczak.arkadiusz@gmail.com>
2025-10-02 19:07:15 +0200

    First Spring Boot basic controller

---
+++ src/main/java/com/Arti/MTG_Wishlist/MtgWishListApplication.java
@@ -0,0 +1,13 @@
+package com.Arti.MTG_Wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgWishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgWishListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Arti/MTG_Wishlist/MtgWishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arti.MTG_Wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgWishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
